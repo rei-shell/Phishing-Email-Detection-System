0d3ce2e7cbbf8918bafda53bfdc3181a9f2b8b55
REISHELL\rache <rachelle3600@gmail.com>
2025-09-20 15:09:00 +0800

    Add some comments

--- main.py
+++ main.py
@@ -12,25 +12,31 @@ from detectKeyword import detectionKeyword
 # Load phishing email datasets
 paths = "dataSets/CEAS_08.csv"
 
+# Read CSV file
 readCsv = readFile(paths)            #Input csv file
 readCsv.read_csv()                     #Read csv file
 #readCsv.print_data()
 
+# Phishing detection
 getEmail = whitelistCheck(readCsv.data)
 getEmail.extract_emailDomain()        #Print sender's domain
 getEmail.check_against_whitelist()
 print(getEmail.get_stats())
 
+# Analyze email subject and body
 getBody = detectionKeyword(readCsv.data)
 getBody.extract_messageBody()
 print('subject:', getBody.analyze_subject(), '\nbody:', getBody.analyze_message())
 
+#Link to Flask web application
 app = Flask(__name__)
 
+# Define routes
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Handle form submission
 @app.route('/submit', methods=['POST'])
 def submit_data():
     sender_email = request.form['sender_email']
@@ -53,5 +59,7 @@ def submit_data():
     result = f"Email Stats: {sender_email}, \nSubject Analysis: {subject}, \nBody Analysis: {body}"
     return render_template("index.html", result=result)
 
+# Run the Flask app
+# When testing your python code, delete this line first.
 if __name__ == '__main__':
     app.run(debug=True)
